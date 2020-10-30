bd349c652fcd2a3105cc5b2b4c641f35f24ed67d
warrenniu <warrenniu@gmail.com>
2020-10-30 13:56:30 -0400

    Completed

---
+++ app/validators/author_validator.rb
@@ -0,0 +1,8 @@
+class AuthorValidator < ActiveModel::Validator
+
+    # def validate(record)
+    #     unless record.author.match?(@name, )
+    #         record.errors[:post] << "This post is invalid" 
+    #     end
+    # end
+end
---
+++ app/validators/post.validator.rb
@@ -0,0 +1,14 @@
+class PostValidator < ActiveModel::Validator
+
+    # def validate(record)
+    #     unless record.post.include?("Won't Believe", "Secret", "Top", or "Guess")
+    #         record.errors[:post] << "This post is invalid" 
+    #     end
+    # end
+
+end
+
+
+
+
+
